80eb9e396aaea384f583a8bb34dffa66724bff9c
Russell <russelldev@protonmail.com>
2021-01-17 14:36:29 +0000

    Add drag function

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const fill = document.querySelector('.fill');
-const filemptiesl = document.querySelectorAll('.empty');
+const empties = document.querySelectorAll('.empty');
+
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+fill.addEventListener('dragdrop', dragDrop);
+
+for (const empty of empties) {
+    fill.addEventListener('dragover', dragOver);
+    fill.addEventListener('dragenter', dragEnter);
+    fill.addEventListener('dragleave', dragLeave);
+    fill.addEventListener('drop', dragDrop);
+
+}
 
 function dragStart() {
+    this.className = this.className + ' hold';
+    setTimeout(() => {
+        this.className = this.className + 'invisible';        
+    }, 0);
     
 }
 
