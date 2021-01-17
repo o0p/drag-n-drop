b6b4f61546fe8853ac93865a11ee595c1ae2220d
Russell <russelldev@protonmail.com>
2021-01-17 14:14:25 +0000

    Add drag-n-drop functions presets

---
+++ app.js
@@ -0,0 +1,23 @@
+function dragStart() {
+    
+}
+
+function dragEnd() {
+
+} 
+
+function dragOver() {
+
+}
+
+function dragEnter() {
+
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+
+}
